c3452c73839873fc96814dcd66a9f571b449b407
Dafetite <ogagadafetite.com>
2023-06-16 21:29:48 +0100

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: 0 for succes
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		if (ch != 0)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(ch + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
