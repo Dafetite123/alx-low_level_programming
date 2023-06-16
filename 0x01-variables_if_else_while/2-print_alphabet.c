64616b0ede128e2f53a57eab7b221a8005ba15aa
Dafetite <ogagadafetite.com>
2023-06-16 20:28:11 +0100

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: 0 for succes
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
