e86fa33b5242f7d0abc1b2222f19e3376c2023cf
Dafetite <ogagadafetite.com>
2023-06-16 14:09:08 +0100

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,8 +15,8 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	
-	r = n%10;
+
+	r = n % 10;
 
 	if ((r < 6) && (r != 0))
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, r);
