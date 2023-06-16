c9a85e8c8d69ca015fcef853bee873f83dc5c5e8
Dafetite <ogagadafetite.com>
2023-06-16 20:33:32 +0100

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: 0 for succes
+ */
+
+int main(void)
+{
+	char ch, cha;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (cha = 'A'; cha <= 'Z'; cha++)                                                   {                                                                                         putchar(cha);                                                              }
+	putchar('\n');
+
+	return (0);
+}
