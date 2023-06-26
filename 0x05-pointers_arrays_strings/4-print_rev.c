18c1a594701fb297d4ef8261de7500524e4a7e63
Dafetite <ogagadafetite.com>
2023-06-26 15:45:30 +0100

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,7 @@
 
 void print_rev(char *s)
 {
-	int cnt = 0, ind;
+	int cnt = 1, ind;
 
 	while (*s)
 	{
@@ -16,9 +16,9 @@ void print_rev(char *s)
 		s++;
 	}
 
-	/* printf("cnt=%d\n", cnt); */
+	printf("cnt=%d\n", cnt);
 
-	for (ind = cnt - 1; ind > 0; ind--, s--)
+	for (ind = cnt; ind > 0; ind--, s--)
 	{
 		_putchar(*s);
 	}
