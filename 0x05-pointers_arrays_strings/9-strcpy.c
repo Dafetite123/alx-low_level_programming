a0f9d0f773f150e36ba4ffece57633349b3488bd
Dafetite <ogagadafetite.com>
2023-06-27 00:56:06 +0100

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strcpy - function
+ * @src: source
+ * @dest: destination
+ * Return: str
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	/* int cnt = 0;
+	char *tmp = src; */
+
+	dest = src;
+	/* while (*tmp)
+	{
+		cnt++;
+		tmp++;
+	} */
+	return (dest);
+}
