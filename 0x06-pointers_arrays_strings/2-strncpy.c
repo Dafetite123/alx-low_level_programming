cf41c8200cc2dd4676f9306b51715482a65e6ad2
Dafetite <ogagadafetite.com>
2023-06-29 23:27:25 +0100

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * _strncpy - function
+ * @dest: dest
+ * @src: src
+ * @n: number of bytes
+ * Return: a pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	/* char *d = dest; */
+	int j = 0; /*, i = 0;
+
+	while (d[i] != '\0')
+		i++; */
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	dest[j] = '\0';
+
+	return (dest);
+}
