1ca1bf48906db8e1c8d3a1d1c0ea78da3b17d9e0
Dafetite <ogagadafetite.com>
2023-06-30 13:16:52 +0100

    7-1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,13 +8,13 @@
 
 char *leet(char *ch)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 	char *let = "aAeEoOtTlL";
 	char *num = "4433007711";
 
 	while (ch[i])
 	{
-		for (; let[j] != '\0'; j++)
+		for (j = 0; let[j] != '\0'; j++)
 		{
 			if (ch[i] == let[j])
 				ch[i] = num[j];
