eab0727a448d17e4ddd4bf0f4aa6015ca62fd231
Dafetite <ogagadafetite.com>
2023-07-04 00:46:47 +0100

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _memset - function
+ * @s: ptr
+ * @b: const bytes
+ * @n: num
+ * Return: ptr
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int index = 0;
+
+	for (;index < n; index++)
+	{
+		s[index] = b;
+	}
+
+	return (s);
+}
