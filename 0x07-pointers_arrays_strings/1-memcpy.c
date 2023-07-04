a57f96f26dedcc1de3fbb11ae55bfb05a06a0f39
Dafetite <ogagadafetite.com>
2023-07-04 19:00:08 +0100

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _memcpy - function
+ * @src: source
+ * @dest: destination
+ * @n: num
+ * Return: ptr
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index = 0;
+
+	for (;index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+
+	return (dest);
+}
