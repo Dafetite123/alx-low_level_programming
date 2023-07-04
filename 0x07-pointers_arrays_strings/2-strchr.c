d37a28284ed8cb84d6217cbf55f3739a59e51d6a
Dafetite <ogagadafetite.com>
2023-07-04 19:12:49 +0100

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _strchr - function
+ * @s: ptr
+ * @c: char
+ * Return: ptr
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index = 0;
+
+	for (;s[index] != '\0'; index++)
+	{
+		if (s[index] == c)
+		{
+			return (&s[index]);
+		}
+	}
+
+	return (NULL);
+}
