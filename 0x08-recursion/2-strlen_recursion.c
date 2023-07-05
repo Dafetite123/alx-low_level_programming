8938278133bcda7c5e846a6976b6cfdbde6169f2
Dafetite <ogagadafetite.com>
2023-07-05 22:53:39 +0100

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - function
+ * @s: ptr
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0, j = 0;
+
+	if (s[i])
+	{
+		/* _putchar(s[i]); */
+		/* printf("What next!\n"); */
+		j += _strlen_recursion(s + 1);
+		j++;
+		/* _putchar(s[i]); */
+		/* i++; */
+		/* printf("What next!\n"); */
+	}
+	/* else */
+		/* _putchar('\n'); */
+	return (j);
+}
