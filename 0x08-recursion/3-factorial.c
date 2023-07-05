89a773cc70c98197c0e2021099426b791af6aaec
Dafetite <ogagadafetite.com>
2023-07-05 23:38:49 +0100

    3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * factorial - function
+ * @n: int
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	int res;
+
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	
+	res = n * factorial(n - 1);
+	
+	return (res);
+}
