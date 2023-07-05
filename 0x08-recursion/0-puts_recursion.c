cb8cc26d05e13551ca23b3040a701b91f897fc6d
Dafetite <ogagadafetite.com>
2023-07-05 14:53:49 +0100

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,5 +8,15 @@
 
 void _puts_recursion(char *s)
 {
-	printf("%s\n", s);
+	int i = 0;
+
+	if (s[i])
+	{
+		_putchar(s[i]);
+		_puts_recursion(s + 1);
+		/* i++; */
+		/* printf("What next!\n"); */
+	}
+	else
+		_putchar('\n');
 }
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function
+ * @s: ptr
+ */
+
+void _print_rev_recursion(char *s);
+{
+	if (
+	printf("%s\n", s);
+}
