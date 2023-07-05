d4cfe9028e7475b49d9d97ccb9e6f7150fbdfb26
Dafetite <ogagadafetite.com>
2023-07-05 23:50:49 +0100

    4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - function
+ * @n: int
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int res;
+
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	res = x * _pow_recursion(x, y - 1);
+
+	return (res);
+}
