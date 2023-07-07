59a368040c5893ffd8c3d32ec24d59520cefdf5d
Dafetite <ogagadafetite.com>
2023-07-07 23:17:16 +0100

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,14 @@
 
 /**
  * main - function
+ * @argc: num of args
+ * @argv: array of args
  * Return: 0 for succes
  */
 
 int main(__attribute__((unused))int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	
 	return (0);
 }
