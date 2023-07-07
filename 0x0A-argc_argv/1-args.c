2e8dd516d198ceb2700b05df8f9e52a4b3e15ee9
Dafetite <ogagadafetite.com>
2023-07-07 23:07:54 +0100

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: 0 for succes
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
