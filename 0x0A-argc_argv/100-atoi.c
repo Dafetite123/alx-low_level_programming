f4a9859dd867836617f1c097aaf13018de16cfad
Dafetite <ogagadafetite.com>
2023-07-08 12:26:21 +0100

    mul

---
+++ 0x0A-argc_argv/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * _atoi - function
+ * @s: ptr
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i, j, k = 0, t = 10, res = 0, m = 1;
+
+	while (s[k])
+		k++;
+
+	/* printf("k = %d\n", k); */
+
+	/* printf(" >%d< ", m); */
+
+	for (i = 0 ; i < k ; i++)
+	{
+		if (s[i] == '-')
+			m = -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			j = s[i] - '0';
+			res = res * t + j;
+			/* res *= t; */
+			/* res += j; */
+		}
+	}
+	res *= m;
+	return (res);
+}
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - function
@@ -13,7 +14,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		i = atoi(argv[1]) * atoi(argv[2]);
+		i = _atoi(argv[1]) * _atoi(argv[2]);
 		printf("%d\n", i);
 	}
 	else
