795501789c80dbfc1e61cc6849220a54471ef031
Dafetite <ogagadafetite.com>
2023-07-11 14:42:08 +0100

    s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strdup - function
+ * @str: ptr
+ * Return: ptr
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i, j = 0;
+
+	while (str[j])
+		j++;
+
+	ptr = malloc(j * sizeof(char));
+	if (ptr == NULL || j == 0)
+		return (NULL);
+
+	for (i = 0 ; i < j ; i++)
+		ptr[i] = str[i];
+
+	return (ptr);
+}
