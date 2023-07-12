e459d085d3f49c37d782a0a3ba58b1668b249fe0
Dafetite <ogagadafetite.com>
2023-07-12 16:44:52 +0100

    g

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * alloc_grid - function
+ * @width: ptr1
+ * @height: ptr2
+ * Return: ptr
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr, i, m;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	ptr = malloc(height * sizeof(int *));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < height ; i++)
+	{
+		ptr[i] = malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
+			return (NULL);
+
+		for (m = 0 ; m < width ; m++)
+			ptr[i][m] = 0;;
+	}
+
+	return (ptr);
+}
